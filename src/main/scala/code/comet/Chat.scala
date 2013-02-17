6e05715aa818931ffccca65dd059bcd11cda67d7
Jeremy Heiner <ScalaProtractor@gmail.com>
2013-02-17 17:16:28 -0500

    fixed the many basic broken things about the chat demo.
    use a stateful comet actor to hold the user id. the input must be
    part of the comet actor so messages can be associated to a user.
    blank messages get dropped. all messages are trimmed (spaces would
    only matter if they were wrapped in a <pre>, which they aren't).
    the whole chat history isn't resent each time, only the new messages.
    the template is much cleaner and open to tweaking by web designers.

--- src/main/scala/code/comet/Chat.scala
+++ src/main/scala/code/comet/Chat.scala
@@ -1,38 +1,82 @@
 
 package code.comet
 
-import scala.xml.Elem
-import net.liftweb.http.{CometActor,CometListener,ListenerManager,RenderOut}
+import scala.xml.{Elem,NodeSeq}
+import net.liftweb.common.{Box,Full,Empty}
+import net.liftweb.http.{SHtml,CometListener,ListenerManager,RenderOut}
+import net.liftweb.http.{StatefulComet,CometState,DeltaTrait}
 import net.liftweb.actor.LiftActor
+import net.liftweb.http.js.{JsCmd,JsCmds}
 
-case class Gol(sgsm:List[String])
+case class Message( user:String, text:String, num:Int = -1 )
+extends DeltaTrait
 {
-  def +(s:String) = Gol( s :: sgsm )
+  def toJs:JsCmd = JsCmds.Run( "ChatAppend("+user+",'"+text+"');" )
 
-  def lis = sgsm.foldLeft(List.empty[Elem]) {
-	(r,m) => <li>{m}</li> :: r }
+  def trimThenIfNonEmpty( action:Message=>Unit ) {
+	val trimmed = text.trim
+	if ( trimmed.nonEmpty ) action(
+	  if ( trimmed.length == text.length ) this
+	  else Message( user, trimmed, num ) ) }
 }
 
-class Chat extends CometActor with CometListener
+object History
 {
-  private var gol = Gol(Nil)
+  val empty = History( Nil )
+  val reset:JsCmd = JsCmds.Run( "ChatReset();" )
+}
+case class History( messages:List[Message] )
+extends CometState[Message,History]
+with net.liftweb.common.Logger
+{
+  def size = if ( messages.isEmpty ) 0 else messages.head.num
+
+  def +( m:Message ) = History( Message( m.user, m.text, size+1 ) :: messages )
+
+  def -( previous:History ):Seq[Message] = {
+	var (since,shared) = messages.splitAt( size - previous.size )
+	if ( shared != previous.messages ) error( "!!! state difference wrong" )
+	since.reverse }
+
+  def render:NodeSeq = {
+  	error( "!!! render should not be called, Chat renders the history" )
+  	NodeSeq.Empty }
+
+  def toJs:JsCmd =
+	messages.foldRight( History.reset ) {
+	  ( m, j ) => JsCmds.CmdPair( j, m.toJs ) }
+}
+
+class Chat
+extends StatefulComet with CometListener
+{
+  type Delta = Message
+  type State = History
+  def emptyState = History.empty
+  def testState( in:Any ):Box[State] = in match {
+	case g:History => Full( g )
+	case _ => Empty }
 
   def registerWith = ChatServer
+  val user = ChatServer.userCount.incrementAndGet.toString
+  def sendMessage( text:String ) =
+	Message( user, text ).trimThenIfNonEmpty( ChatServer.! )
 
-  override def lowPriority = {
-	case x:Gol => gol = x ; reRender() }
-
-  def render = <ul>{ gol.lis }</ul>
+  override def render:RenderOut = Seq(
+	SHtml.ajaxForm( SHtml.text( "", sendMessage, "id" -> "ChatInput" ),
+					JsCmds.Noop, JsCmds.Run( "ChatInput.value = '';" ) ),
+	JsCmds.Script( JsCmds.CmdPair(
+	  JsCmds.Run( "ChatUser="+user+";" ), state.toJs )) )
 }
-object ChatServer extends LiftActor with ListenerManager
+object ChatServer
+extends LiftActor with ListenerManager
 {
-  private var gol = Gol(List("Welcome"))
+  val userCount = new java.util.concurrent.atomic.AtomicInteger
 
-  def createUpdate = gol
+  private var history = History.empty + Message( "0", "Welcome" )
+  private def addMessage( m:Message ) { history += m ; updateListeners() }
+  def createUpdate = history
 
   override def lowPriority = {
-	case s:String =>
-	  val t = s.trim
-	  if ( t.nonEmpty ) {
-		gol += s ; updateListeners() } }
+	case m:Message => m.trimThenIfNonEmpty( addMessage ) }
 }
