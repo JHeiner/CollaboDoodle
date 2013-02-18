5404756c5c63b589f6988b4490c8abad896f590d
Jeremy Heiner <ScalaProtractor@gmail.com>
2013-02-18 11:38:32 -0500

    implement /trace and cleanup repeated trimming.

--- src/main/scala/code/comet/Chat.scala
+++ src/main/scala/code/comet/Chat.scala
@@ -10,16 +10,11 @@ import net.liftweb.util.AltXML
 import net.liftweb.http.js.{JsCmd,JsCmds,JE}
 import JsCmds.jsExpToJsCmd
 
-case class Message( user:String, nick:String, text:String, num:Int = -1 )
+case class Message( user:String, command:String, nick:String, text:String,
+                    num:Int = -1 )
 extends DeltaTrait
 {
-  def toJs:JsCmd = JE.Call( "ChatAppend", user, nick, text )
-
-  def trimThenIfNonEmpty( action:Message=>Unit ) {
-	val trimmed = text.trim
-	if ( trimmed.nonEmpty ) action(
-	  if ( trimmed.length == text.length ) this
-	  else Message( user, nick, trimmed, num ) ) }
+  def toJs:JsCmd = JE.Call( "ChatAppend", user, command, nick, text )
 }
 
 object History
@@ -33,22 +28,23 @@ with net.liftweb.common.Logger
 {
   def size = if ( messages.isEmpty ) 0 else messages.head.num
 
-  def +( m:Message ) = History(
-	Message( m.user, m.nick, m.text, size+1 ) :: messages )
+  def +( m:Message ) = History( m.copy( num = size+1 ) :: messages )
 
   def -( previous:History ):Seq[Message] = {
-	var (since,shared) = messages.splitAt( size - previous.size )
-	if ( shared != previous.messages ) error( "!!! state difference wrong" )
-	since.reverse }
+    var (since,shared) = messages.splitAt( size - previous.size )
+    if ( shared != previous.messages ) error( "!!! state difference wrong" )
+    since.reverse }
 
   def render:NodeSeq = {
-  	error( "!!! render should not be called, Chat renders the history" )
-  	NodeSeq.Empty }
+    error( "!!! render should not be called, Chat renders the history" )
+    NodeSeq.Empty }
 
   def toJs:JsCmd =
-	messages.foldRight( History.reset ) {
-	  ( m, j ) => JsCmds.CmdPair( j, m.toJs ) }
+    messages.foldRight( History.reset ) {
+      ( m, j ) => JsCmds.CmdPair( j, m.toJs ) }
 }
+
+case class Input( chat:Chat, dangerous:String )
 
 class Chat
 extends StatefulComet with CometListener
@@ -57,58 +53,83 @@ extends StatefulComet with CometListener
   type State = History
   def emptyState = History.empty
   def testState( in:Any ):Box[State] = in match {
-	case g:History => Full( g )
-	case _ => Empty }
+    case g:History => Full( g )
+    case _ => Empty }
 
   def registerWith = ChatServer
   val user = ChatServer.userCount.incrementAndGet.toString
   var nick = ""
-  def trimThenHandle( input:String ) = {
-	val escaped = new StringBuilder( input.length )
-	AltXML.toXML( Text(input), null, escaped, false, false )
-	var i = escaped.length - 1
-	while ( i >= 0 ) { escaped.charAt( i ) match {
-	  // see owasp.org XSS prevention cheat sheet...
-	  case '\'' => escaped.replace( i, i+1, "&#x27;" )
-	  case '/' => escaped.replace( i, i+1, "&#x2F;" )
-	  case _ => } ; i -= 1 }
-	val text = escaped.result.trim
-	i = text.indexOf(' ')
-	if ( i < 0 ) i = text.length
-	val before = text.substring( 0, i ).toLowerCase
-	val after = text.substring( i ).trim
-	if ( before match {
-	  case "&#x2f;nick" =>
-		val from = if ( nick.nonEmpty ) (" "+nick) else ""
-		val into = if ( after == nick ) "" else (" \u27a1 "+after)
-		ChatServer ! Message( "0", "/nick", user+from+into )
-		nick = after
-		false
-	  case "&#x2f;time" =>
-		if ( after.nonEmpty ) true else {
-		  ChatServer ! ChatServer.timeStamp
-		  false }
-	  case _ => true }) ChatServer ! Message( user, nick, text ) }
+  def sendInput( s:String ) { ChatServer ! Input( this, s ) }
 
   override def render:RenderOut = Seq(
-	SHtml.ajaxForm( SHtml.text( "", trimThenHandle, "id" -> "ChatInput" ),
-					JsCmds.Noop, JsCmds.SetValById( "ChatInput", "" ) ),
-	JsCmds.Script( JsCmds.CmdPair(
-	  JsCmds.SetExp( JE.JsRaw( "ChatUser" ), user ), state.toJs )) )
+    SHtml.ajaxForm( SHtml.text( "", sendInput, "id" -> "ChatInput" ),
+                    JsCmds.Noop, JsCmds.SetValById( "ChatInput", "" ) ),
+    JsCmds.Script( JsCmds.CmdPair(
+      JsCmds.SetExp( JE.JsRaw( "ChatUser" ), user ), state.toJs )) )
 }
 object ChatServer
 extends LiftActor with ListenerManager
 {
   val userCount = new java.util.concurrent.atomic.AtomicInteger
+
+  def userMessage( chat:Chat, text:String ) =
+    Message( chat.user, "", chat.nick, text )
+  def adminMessage( command:String, user:String, info:String ) =
+    Message( "0", command, user, info )
+
   val dateRFC822 = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
   val dateSimple = new java.text.SimpleDateFormat( dateRFC822 )
   def dateCurrent = dateSimple.format( new java.util.Date )
-  def timeStamp = Message( "0", "/time", dateCurrent )
+  def timeMessage( user:String ) = adminMessage( "/time", user, dateCurrent )
 
-  private var history = History.empty + timeStamp
+  def traceMessage( user:String ) = {
+    import java.net.NetworkInterface._
+    import scala.collection.JavaConversions._
+    val (n4,n6) = ( for {
+      i <- getNetworkInterfaces
+      if ! i.isLoopback
+      a <- i.getInetAddresses }
+      yield a ).toList.partition{ _.isSiteLocalAddress }
+    val ns = ( if ( n4.nonEmpty ) n4 else n6 ).map{ _.getHostAddress }
+    adminMessage( "/trace", user, ns.mkString( ", " ) ) }
+
+  private var history = History.empty + timeMessage("0") + traceMessage("0")
   private def addMessage( m:Message ) { history += m ; updateListeners() }
   def createUpdate = history
 
   override def lowPriority = {
-	case m:Message => m.trimThenIfNonEmpty( addMessage ) }
+    case Input( chat, dangerous ) =>
+      val trimmed = escape( dangerous ).trim
+      var space = trimmed.indexOf(' ')
+      if ( space < 0 ) space = trimmed.length
+      val before = trimmed.substring( 0, space ).toLowerCase
+      val after = trimmed.substring( space ).trim
+      val admin = before match {
+        case "&#x2f;nick" =>
+          val nick = chat.nick
+          val into = if ( after == nick ) "" else (" \u27a1 "+after)
+          if ( into.nonEmpty ) chat.nick = after
+          Some( adminMessage( "/nick", chat.user, nick+into ) )
+        case "&#x2f;time" =>
+          if ( after.nonEmpty ) None else
+            Some( timeMessage( chat.user ) )
+        case "&#x2f;trace" =>
+          if ( after.nonEmpty ) None else
+            Some( traceMessage( chat.user ) )
+        case _ => None }
+      if ( admin.nonEmpty )
+        addMessage( admin.get ) // admin might have empty text
+      else if ( trimmed.nonEmpty ) // but empty user messages are dropped
+        addMessage( userMessage( chat, trimmed ) ) }
+
+  def escape( dangerous:String ) = {
+    val builder = new StringBuilder( dangerous.length )
+    AltXML.toXML( Text(dangerous), null, builder, false, false )
+    var i = builder.length - 1
+    while ( i >= 0 ) { builder.charAt( i ) match {
+      // see owasp.org XSS prevention cheat sheet...
+      case '\'' => builder.replace( i, i+1, "&#x27;" )
+      case '/' => builder.replace( i, i+1, "&#x2F;" )
+      case _ => } ; i -= 1 }
+    builder.result }
 }
